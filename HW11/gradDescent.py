019ac0864ed8ebe94fcef3d825514dd48d67e860
Benzoin96485 <1900011804@pku.edu.cn>
2022-04-11 18:14:54 +0800

    HW11

---
+++ HW11/gradDescent.py
@@ -0,0 +1,84 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class GD:
+    def __init__(self, A: np.array, x0: np.array, alpha: float, beta: float, eta: float):
+        self.A = A
+        self.x0 = x0
+        self.alpha = alpha
+        self.beta = beta
+        self.x = x0
+        self.eta2 = eta ** 2
+        self.fs = []
+        self.sls = []
+        self.L = 0
+
+    def f(self, x: np.array):
+        # the i-th row of A is a_i^T
+        return np.sum(np.exp(self.A @ x) + np.exp(-self.A @ x))
+
+    def df(self, x: np.array):
+        return (np.exp(self.A @ x) - np.exp(-self.A @ x)) @ self.A
+
+    def _step(self):
+        dx = -self.df(self.x)
+        df2 = np.dot(dx, dx)
+        if df2 < self.eta2:
+            return True
+        fx = self.f(self.x)
+        t = 1
+        while self.f(self.x + t * dx) > fx - self.alpha * t * df2:
+            t *= self.beta
+        self.x = self.x + t * dx
+        self.fs.append(fx)
+        self.sls.append(t * np.sqrt(df2))
+        return False
+
+    def opt(self):
+        while True:
+            if self._step():
+                break
+        self.L = len(self.fs)
+    
+    def plot_fi(self):
+        plt.figure()
+        plt.plot(range(self.L), self.fs)
+        plt.show()
+
+    def plot_sli(self):
+        plt.figure()
+        plt.plot(range(self.L), self.sls)
+        plt.show()
+
+if __name__ == "__main__":
+    plt.figure()
+    for seed in range(114514, 114517):
+        np.random.seed(seed)
+        A = np.random.randn(3,3)
+        print(A)
+        x0 = np.random.randn(3)
+        print(x0)
+        betas = np.linspace(0.01, 0.99, 99)
+        Ls = []
+        for beta in betas:
+            gd = GD(A, x0, alpha=0.5, beta=beta, eta=1e-6)
+            gd.opt()
+            Ls.append(gd.L)
+        plt.plot(betas, Ls)
+    plt.show()
+    plt.figure()
+    for seed in range(114514, 114517):
+        np.random.seed(seed)
+        A = np.random.randn(3,3)
+        print(A)
+        x0 = np.random.randn(3)
+        print(x0)
+        alphas = np.linspace(0.01, 0.99, 99)
+        Ls = []
+        for alpha in alphas:
+            gd = GD(A, x0, alpha=alpha, beta=0.9, eta=1e-6)
+            gd.opt()
+            Ls.append(gd.L)
+        plt.plot(alphas, Ls)
+    plt.show()
